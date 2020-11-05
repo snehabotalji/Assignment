091f38f4c949bf19ea7c54188c3487f4db1968fb
sneha <botaljis@gmail.com>
2020-11-05 18:56:02 +0530

    first step

---
+++ Project/src/StringCal.java
@@ -0,0 +1,23 @@
+public class StringCal {
+	
+int Add(String numbers)
+{
+	int size= numbers.length();
+	if(size!=0)
+	{
+		String s1[] =numbers.split(",");
+		int sum = 0;
+		for(int i=0;i<s1.length;i++)
+		{
+			sum=sum+Integer.parseInt(s1[i]);
+		}
+		return sum;
+	}
+	return 0;
+
+}
+public static void main(String[] args) {
+	StringCal  scal=new StringCal();
+	System.out.println(scal.Add("1,2"));
+}
+}
